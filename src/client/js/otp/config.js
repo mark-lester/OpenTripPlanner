d4edc6c9bfadb1d8e22d1a591ef8de4baa6cf271
Mark Lester <mc_lester@yahoo.co.uk>
2014-11-11 19:31:11 +0000

    nice name

--- src/client/js/otp/config.js
+++ src/client/js/otp/config.js
@@ -108,7 +108,7 @@ otp.config = {
      * Site name / description / branding display options
      */
 
-    siteName            : "Sri Lanka - Crowd Sourced Route Planner",
+    siteName            : "WikiTimeTable Sri Lanka - Crowd Sourced Route Planner",
     siteUrl            : "http://www.wikitimetable.com/",
     siteDescription     : "An OpenTripPlanner deployment.",
     logoGraphic         : 'images/cow-icon.png',
